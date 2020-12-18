1c9ec7430ac9f88893ee7d3b01347f038df69ec0
Alexandre MARY <alexandre.mary@meteo.fr>
2020-12-18 17:34:53 +0100

    Gallery inputs recuperation scripts

---
+++ epygram/doc_sphinx/source/gallery/get_inputs.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+HOST=ftp.umr-cnrm.fr
+PORT=21
+LOGIN=epygram  # read-only anonymous user
+PASSWORD=EpygraM  # with anonymous password
+INPUTS_DIR=inputs
+
+if [ ! -d "$INPUTS_DIR" ]
+then
+    mkdir $INPUTS_DIR
+fi
+
+cd $INPUTS_DIR
+ftp -i -v -n $HOST $PORT << EOF
+quote USER $LOGIN
+quote PASS $PASSWORD
+bin
+prompt
+get analysis.full-arpege.tl149-c24.fa
+get glob01.grib
+get grid.arome-forecast.guyane0025+0000:00.grib
+get grid.arome-forecast.guyane0025+0012:00.grib
+get grid.arome-forecast.guyane0025+0024:00.grib
+get ic.full-surfex.corsica-02km50.fa
+get ICMSHAROM+0022
+get pgd.albachir-07km50.SAND.fa
+quit
+EOF
+cd ..
