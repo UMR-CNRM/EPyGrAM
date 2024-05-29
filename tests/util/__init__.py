4036d1bbc340f4090717d0196cbe20f0631f4973
CHIKHI Walid <waliidchikhi@gmail.com>
2024-05-29 09:38:19 +0200

    adapt tests to be run with pytest from rootdir

--- tests/util/__init__.py
+++ tests/util/__init__.py
@@ -4,8 +4,13 @@
 # This software is governed by the CeCILL-C license under French law.
 # http://www.cecill.info
 from __future__ import print_function, absolute_import, division, unicode_literals
+import os
 
-datadir = './data'
+file_dir = os.path.dirname(os.path.abspath(__file__))
+
+datadir = os.path.join(file_dir, '..', 'data')
+
+datadir = os.path.normpath(datadir)
 
 delta_assertAlmostEqual = 1e-12
 delta_assertAlmostEqual4pyproj = 1e-7  # FIXME: because pyproj not reproducible between python2 and python3
